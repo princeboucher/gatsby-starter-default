2ee516f2a07e1e1929f45139180afc4fddf3662c
Prince <736187+princeboucher@users.noreply.github.com>
2019-10-06 21:40:39 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Prince Boucher`,
+    description: `Prince Boucher is a creative technologist focused on balancing civic, media, and technology.`,
+    author: `@princeboucher`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
