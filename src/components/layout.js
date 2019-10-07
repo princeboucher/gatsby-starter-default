7c175c4bb3b78def14a02fb407c49babe7b31206
Prince <736187+princeboucher@users.noreply.github.com>
2019-10-06 21:42:07 -0700

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,9 +36,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built in
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://en.wikipedia.org/wiki/Mission_District,_San_Francisco">The Mission</a>
         </footer>
       </div>
     </>
