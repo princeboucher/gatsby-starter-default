c7061c2e647543e930683c991bf336edfc0df306
princeboucher <736187+princeboucher@users.noreply.github.com>
2020-06-20 11:26:40 -0700

    Update index.js
    takeaway tech

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Prince Boucher" />
-    <h1>I'm a creative technologist.</h1>
+    <h1>I'm a creative.</h1>
     <p>I'm inspired with collaborating with activists, creatives, and technologists at the intersection of community, media, and technology.</p>
     <p>I've been running <a href="https://www.followtheprince.com">Follow The Prince</a> for over ten years working with brands like 300 Entertainment, AWS, Interscope, Funny or Die, Fullscreen, and UnitedHealth. In between I've been a first marketing hire at LittleCast, Joyus, and Fin.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
