57a98eb7fcee5e7afac8e4f90c0be7644717aa7e
princeboucher <736187+princeboucher@users.noreply.github.com>
2020-07-10 10:08:31 -0700

    header change
    blue to black!

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `blue`,
+      background: `black`,
       marginBottom: `1.45rem`,
     }}
   >
