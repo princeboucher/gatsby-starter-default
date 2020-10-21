702c6bb0c607f61f91cdfa5a46191fb88721ce75
princeboucher <736187+princeboucher@users.noreply.github.com>
2020-10-20 20:26:55 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <h1>Applied Generalist.</h1>
     <p>Inspiring activists, creatives, and technologists at the intersection of civic, media, and technology peak my curiousity.</p>
     <p><a href="https://www.followtheprince.com">Follow The Prince</a> is where I've worked on projects with 300 Entertainment, AWS, Interscope, Funny or Die, Fullscreen, and UnitedHealth. In between I've been a content lead at LittleCast, Joyus, and Fin.</p>
-    <p>Over the past couple of years I've joined several communities that have been helpful in my development and growth as a pesrson and entrepreneur. Special shout out to the <a href="www.globalshapers.org">Global Shapers Community</a>, <a href="www.fedtech.io">FedTech</a>, <a href="www.beondeck.com">On Deck</a>, and <a href="www.southparkcommons.com">South Park Commons</a>.</p>
+    <p>Over the past couple of years I've joined several communities that have been helpful in my development and growth as a person and entrepreneur. Special shout out to the <a href="www.globalshapers.org">Global Shapers Community</a>, <a href="www.fedtech.io">FedTech</a>, <a href="www.beondeck.com">On Deck</a>, and <a href="www.southparkcommons.com">South Park Commons</a>.</p>
     <p>Lately, I'm social distancing with my mask and mostly volunteering at <a href="https://www.usdigitalresponse.org">U.S. Digital Response.</a> </p>
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
