d5ef80c14e1c7c988084d02eb307b0ae4c3dc00c
princeboucher <736187+princeboucher@users.noreply.github.com>
2020-12-25 00:20:55 -0800

    Update index.js
    dropped<br> and lowercase p

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ const IndexPage = () => (
     <p><a href="https://www.followtheprince.com">Follow The Prince</a> is where I've worked on projects with 300 Entertainment, AWS, Interscope, Funny or Die, Fullscreen, and UnitedHealth. In between I've been a content lead at LittleCast, Joyus, and Fin.</p>
     <p>Over the past couple of years I've joined several communities that have been helpful in my development and growth as a person and entrepreneur. Special shout out to the <a href="www.globalshapers.org">Global Shapers Community</a>, <a href="www.fedtech.io">FedTech</a>, <a href="www.beondeck.com">On Deck</a>, and <a href="www.southparkcommons.com">South Park Commons</a>.</p>
     <p>Lately, I'm social distancing with my mask and mostly volunteering at <a href="https://www.usdigitalresponse.org">U.S. Digital Response.</a>
-    <P>
-Thanks for stopping by!<br>
+    <p>
+Thanks for stopping by!
     </p>
 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
